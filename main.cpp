60cdf4b8c8dacd6e719c6b69dff2152403beebeb
Benjamin Altieri <benaltieri@gmail.com>
2017-10-10 22:25:58 -0700

    Snapshot at the end of Chapter 4 - https://www.johnlamp.net/cmake-tutorial-4-libraries-and-subdirectories.html

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+#include "ToDoCore/ToDo.h"
+
+#define EXPECT_EQUAL(test, expect) equalityTest( test, expect, \
+                                                 #test, #expect, \
+                                                 __FILE__, __LINE__)
+template <typename T1, typename T2>
+int equalityTest(const T1 testValue,
+                 const T2 expectedValue,
+                 const char* testName,
+                 const char* expectedName,
+                 const char* fileName,
+                 const int lineNumber
+                );
+
+using namespace std;
+
+int main(int, char**)
+{
+    int result = 0;
+
+    ToDo list;
+
+    list.addTask("write code");
+    list.addTask("compile");
+    list.addTask("test");
+
+    result |= EXPECT_EQUAL(list.size(), (size_t)3);
+    result |= EXPECT_EQUAL(list.getTask(0), "write code");
+    result |= EXPECT_EQUAL(list.getTask(1), "compile");
+    result |= EXPECT_EQUAL(list.getTask(2), "test");
+
+    if (result == 0) {
+        cout << "Test passed" << endl;
+    }
+
+    return 0;
+}
+
+template <typename T1, typename T2>
+int equalityTest(const T1 testValue,
+                 const T2 expectedValue,
+                 const char* testName,
+                 const char* expectedName,
+                 const char* fileName,
+                 const int lineNumber
+                )
+{
+    if (testValue != expectedValue) {
+        cerr << fileName << ":" << lineNumber << ": "
+             << "Expected " << testName << " "
+             << "to equal " << expectedName << " (" << expectedValue << ") "
+             << "but it was (" << testValue << ")" << endl;
+
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
