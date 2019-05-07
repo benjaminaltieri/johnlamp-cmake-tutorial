4a82f609366ce6eb60779f2376144de4c3d9503f
Benjamin Altieri <benaltieri@gmail.com>
2019-05-07 14:56:05 -0700

    use stablecoder implementation

--- travis.sh
+++ travis.sh
@@ -6,7 +6,7 @@ mkdir -p build && cd build
 
 build() {
     # Configure
-    cmake -DENABLE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+    cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
     # Build (for Make on Unix equivalent to `make -j $(nproc)`)
     cmake --build . --config Debug -- -j $(nproc)
     # Test
@@ -14,19 +14,7 @@ build() {
 }
 
 do_coverage_report() {
-    # Create lcov report
-    # capture coverage info
-    #lcov --directory . --capture --output-file coverage.info
-    # filter out system and extra files.
-    # To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
-    #lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
-    # output coverage data for debugging (optional)
-    #lcov --list coverage.info
-    # Uploading to CodeCov
-    # '-f' specifies file(s) to use and disables manual coverage gathering and file search which has already been done above
-    # ignore for know
-    #bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
-    return 0
+    cmake --build . --config Debug -- ccov-all -j $(nproc)
 }
 
 run() {
